04be971addcf3894c44ba055514c1070c863df46
Nischitha07 <71720078+Nischitha07@users.noreply.github.com>
2020-12-01 20:00:36 +0530

    Add files via upload

---
+++ main (1).js
@@ -0,0 +1,14 @@
+function preload(){
+
+}
+function setup(){
+    canvas = createCanvas(500 , 400);
+    canvas.center();
+}
+function draw(){
+
+}
+function take_snapshot()
+{
+    save("Filter_snapshot.png");
+}
